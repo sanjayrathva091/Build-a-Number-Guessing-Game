764ca86b8739c7d377d85ae30634245c5ead1dbc
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-02-10 07:06:49 +0000

    feat: generate random number and input username

--- number_guess.sh
+++ number_guess.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Connect to the database
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
+# Prompt for username
 echo "Enter your username:"
 read username
+
+# Generate a random number between 1 and 1000
+secret_number=$(( RANDOM % 1000 + 1 ))
+
+# Escape single quotes in the username for SQL queries
+escaped_username=$(echo "$username" | sed "s/'/''/g")
