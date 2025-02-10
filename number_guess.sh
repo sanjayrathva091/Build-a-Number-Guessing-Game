e92c2b7c4f1882ac61324baff1070216927bfeae
Sanjaykumar Rathva <sanjayrathva091@gmail.com>
2025-02-10 07:18:13 +0000

    feat: validate user input

--- number_guess.sh
+++ number_guess.sh
@@ -22,10 +22,23 @@ if [[ -z $user_exists ]]; then
   echo "Welcome, $username! It looks like this is your first time here."
 else
   # If the user exists, retrieve their game statistics
-  
+  games_played=$($PSQL "SELECT games_played FROM users WHERE username='$escaped_username'")
+  best_game=$($PSQL "SELECT best_game FROM users WHERE users.username='$escaped_username'")
+  echo "Welcome back, $username! You have played $games_played games, and your best game took $best_game guesses."  
 fi
 
 
 # Start the guessing game
 number_of_guesses=0
 echo "Guess the secret number between 1 and 1000:"
+
+while true; do
+  read guess
+
+  # Validate the input is an integer
+  if [[ ! $guess =~ ^[0-9]+$ ]]; then
+    echo "That is not an integer, guess again:"
+    continue
+  fi
+  
+done
